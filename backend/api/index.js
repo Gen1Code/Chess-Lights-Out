33ec9154b265f9b8a8d2aa8d57e276f72bc1c7f5
Gen1Code <72555054+Gen1Code@users.noreply.github.com>
2024-06-27 22:01:49 +0100

    test auto deploy

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/hello", (req, res) => res.send("Hello from API From github"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
