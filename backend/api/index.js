d41351b21239b75308c437f2fdd1cc12459be964
Gen1Code <72555054+Gen1Code@users.noreply.github.com>
2024-06-29 15:59:51 +0100

    link backend and db

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
+const { db } = require('@vercel/postgres');
 const express = require("express");
+
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/hello", (req, res) => res.send("Hello from API"));
 app.get("/test", (req, res) => res.send("Test"));
+
+app.get("/testdb", async(req, res) => {
+    const result = await db.query("SELECT * FROM users LIMIT 5");
+    res.json(result.rows);
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
