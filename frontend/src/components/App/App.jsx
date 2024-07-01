a7be513798a212e3e78e838e261307d33e62c0f5
Gen1Code <72555054+Gen1Code@users.noreply.github.com>
2024-07-01 13:41:28 +0100

    FE: chess with moves

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,31 +1,36 @@
-import { useState } from 'react'
-import reactLogo from '@assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from '@assets/react.svg';
+import { Chess } from 'chess.js';
+import { Chessboard } from 'react-chessboard';
+import './App.css';
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [game, setGame] = useState(new Chess());
+
+  function makeAMove(move) {
+    const gameCopy = new Chess(game.fen());
+    const result = gameCopy.move(move);
+    setGame(gameCopy);
+    return result; // null if the move was illegal, the move object if the move was legal
+  }
+  function onDrop(sourceSquare, targetSquare) {
+    const move = makeAMove({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q", // always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    if (move === null) return false;
+    return true;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Chess Lights Out</h1>
+      <Chessboard position={game.fen()} onPieceDrop={onDrop} />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
