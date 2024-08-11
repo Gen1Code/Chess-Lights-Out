a63f03862896d728c33e0f806afebd104aacbb4b
Gen1Code <72555054+Gen1Code@users.noreply.github.com>
2024-08-11 11:55:23 +0100

    semi-working Queue

--- backend/api/lib/pubsub.js
+++ backend/api/lib/pubsub.js
@@ -1,18 +1,82 @@
 import Ably from "ably";
 import dotenv from "dotenv";
+import amqp from "amqplib/callback_api.js";
 
 dotenv.config();
 
-export async function publish(channelName, message) {
+export async function publish(channelName, event, message) {
     const client = new Ably.Realtime(process.env.ABLY_API_KEY);
-    await Ably.connection.once('connected');
     const channel = client.channels.get(channelName);
-    await channel.publish(message);
-    Ably.close();
+    await channel.publish(event, message);
+    client.close();
 }
 
 export async function getAuthTokenRequest(userId) {
-    const ably = new Ably.Rest({ key: process.env.ABLY_API_KEY  });
-    const tokenRequest = await ably.auth.createTokenRequest({ clientId: userId });
+    const ably = new Ably.Rest({ key: process.env.ABLY_API_KEY });
+    const capability = { "*": ["subscribe"] };
+    const tokenParams = {
+        clientId: userId,
+        capability: JSON.stringify(capability),
+    };
+    const tokenRequest = await ably.auth.createTokenRequest(tokenParams);
     return tokenRequest;
+}
+
+export function consumeQueue(queueName) {
+    return new Promise((resolve, reject) => {
+        const url =
+            "amqps://" +
+            process.env.ABLY_API_KEY +
+            "@eu-west-1-a-queue.ably.io:5671/shared";
+
+        amqp.connect(url, (error0, connection) => {
+            if (error0) {
+                return reject(error0);
+            }
+            connection.createChannel((error1, channel) => {
+                if (error1) {
+                    connection.close();
+                    return reject(error1);
+                }
+                channel.prefetch(1);
+
+                const timer = setTimeout(() => {
+                    channel.close(() => {
+                        connection.close();
+                        resolve(null);
+                    });
+                }, 100);
+
+                channel.consume(
+                    queueName,
+                    (msg) => {
+                        if (msg !== null) {
+                            clearTimeout(timer);
+                            const messageContent = msg.content.toString();
+                            channel.ack(msg);
+                            resolve(messageContent);
+                        }
+                    },
+                    {
+                        noAck: false,
+                    }
+                );
+            });
+        });
+    });
+}
+
+export async function getMessageFromQueue(queueName) {
+    try {
+        const message = await consumeQueue(queueName);
+        if (message !== null) {
+            console.log('Received message:', message);
+            return message
+        } else {
+            console.log('No message received within the timeout period.');
+            return null
+        }
+    } catch (error) {
+        console.error('Error consuming queue:', error);
+    }
 }
--- backend/api/routes/game.js
+++ backend/api/routes/game.js
@@ -1,10 +1,25 @@
-import express from 'express';
+import express from "express";
+import { publish, getMessageFromQueue } from "../lib/pubsub.js";
 
 const router = express.Router();
 
-router.post('/play', async (req, res) => {
-    res.json({ message: "looking For a game" });
+let queueNames = ["7lsHZQ:maze-static-normal"];
+
+router.post("/play", async (req, res) => {
+    console.log("settings", req.body);
+
+    const messages = await getMessageFromQueue(queueNames[0]);
+
+    console.log("messages", messages);
+    if (messages !== null) {
+        res.json({ message: "Game Found" });
+    } else {
+        console.log("publishing settings");
+        // Publish the settings to the queue (temp)
+        publish(queueNames[0], 'settings', JSON.stringify(req.body));
+        res.json({ message: "looking For a game" });
+    }
+
 });
-  
 
 export default router;
